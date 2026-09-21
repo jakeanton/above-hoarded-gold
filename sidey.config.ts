c8f8b8793b1626be7e358a131dfa6701b7d6bb9f
Jake Schreuder <jakeanton@gmail.com>
2026-09-21 15:31:19 -0400

    Initial push

--- sidey.config.ts
+++ sidey.config.ts
@@ -9,16 +9,16 @@ export const sideyConfig = {
    */
   site: {
     // The main title displayed in browser tabs and search engine results
-    title: "Astro Sidey",
+    title: "Above Hoarded Gold",
 
     // A short fallback summary of your site used for SEO and social share cards
-    description: "A minimal, content-focused Astro theme for personal sites.",
+    description: "A pencils-paper-and-dice adventure game about journeys and returns.",
 
     // The production domain where your site is deployed (no trailing slash)
-    url: "https://sidey.odhyp.com",
+    url: "https://.com",  //todo
 
     // Your name, utilized in copyright strings and author meta tags
-    author: "Odhy Pradhana",
+    author: "Jake Schreuder",
 
     // The primary language attribute for HTML accessibility engines (e.g., "en", "id")
     locale: "en",
@@ -32,9 +32,13 @@ export const sideyConfig = {
    */
   navigation: [
     { label: "Home", href: "/" },
-    { label: "Writings", href: "/writings" },
-    { label: "About", href: "/about" },
-    { label: "RSS", href: "/rss.xml" },
+    { label: "Character", href: "/character" },
+    { label: "Inventory", href: "/inventory" },
+    { label: "Play", href: "/playing-the-game" },
+    { label: "Referee", href: "/running-the-game" },
+    { label: "Travel", href: "/travel" },
+    // { label: "Writings", href: "/writings" }
+    // { label: "RSS", href: "/rss.xml" },
   ],
 }
 
